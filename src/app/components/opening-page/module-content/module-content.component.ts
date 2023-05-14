321b614253c98a80edb6333efff8ebd5df2f8e1e
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2023-05-13 21:32:57 -0300

    trocado os icones da home
    .

--- src/app/components/opening-page/module-content/module-content.component.ts
+++ src/app/components/opening-page/module-content/module-content.component.ts
@@ -11,6 +11,10 @@ export class ModuleContentComponent {
   @Input() imageCenterPath!: string;
   @Input() textTittle!: string;
   @Input() colorDivisorTittle!: string;
+  @Input() widthImageCenter: string = "240px";
+  @Input() heightImageCenter: string = "234px";
+  @Input() marginLeftImageCenter: string = "10px";
+  @Input() marginTopImageCenter: string = "0px";
 
   @Output() clicked = new EventEmitter();
 }
